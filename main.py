87a2d5f6d4ee6018a5f6e933f15e84d21e82c254
dawtom97 <dawtom97@gmail.com>
2025-11-26 17:46:55 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+
+
+
+a = 10
+
+b = 5
+
+c = 1
+
+# git init - utworzenie lokalnego repozytorium
+# git status - sprawdzenie obecnego wyglądu repo
+
+# git add nazwa_pliku - dodaj plik do repozytorium
+# git add . - dodaj wszystkie pliki i foldery do repo
+
+# git commit -m 'wiadomosc' - wykonanie commita z wiadomością
+
+
